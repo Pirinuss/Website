93f2fcb942ecc621a429863c90d16a99cbd1b231
Marc <marcwendelborn@web.de>
2019-11-09 00:27:38 +0100

    Second try projects

--- src/components/projects/ProjectItem.js
+++ src/components/projects/ProjectItem.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './Projects.css'
+import WebsiteImage from './WebsiteImage.PNG'
 
 class ProjectItem extends React.Component {
     constructor(props) {
+        console.log(props.content);
         super(props);
     }
     render() {
@@ -12,6 +14,14 @@ class ProjectItem extends React.Component {
                     <div className="deco-div" />
                     <h2 className="projectItem-header">{this.props.title}</h2>
                     <div className="deco-div" />
+                </div>
+                <div className="projectItem-body-container">
+                    <div className="projectItem-text-content">
+                        <p>{this.props.content}</p>
+                    </div>
+                    <div className="projectItem-image-content">
+                        <img src={WebsiteImage} />
+                    </div>
                 </div>
             </div>
         );
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,19 +1,38 @@
 import React from 'react';
 import ProjectItem from './ProjectItem.js';
+import InfiniteCarousel from 'react-leaf-carousel';
 import './Projects.css'
+
+const WebsiteContent = 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.';
+const WorkloadGeneratorContent = 'empty';
+const PasswordGeneratorContent = 'empty2';
 
 class Projects extends React.Component {
     render() {
         return (
             <div className="projects">
                 <div className="header-container">
-                    <h2>PROJEKTE</h2>
                 </div>
                 <div className="projects-container">
-                    <ProjectItem title="Website"></ProjectItem>
-                    <ProjectItem title="Password Manager"></ProjectItem>
-                    <ProjectItem title="Workload Generator"></ProjectItem>
-                    <ProjectItem title="WEBSITE"></ProjectItem>
+                    <InfiniteCarousel
+                        breakpoints={[
+                            {
+                              breakpoint: 100,
+                              settings: {
+                                slidesToShow: 1,
+                                slidesToScroll: 4,
+                              },
+                            },
+                          ]} 
+                        dots={true}
+                        slidesToScroll={1}
+                        slidesToShow={1}
+                        scrollOnDevice={true}>
+                        <ProjectItem title="Website" content={WebsiteContent}></ProjectItem>
+                        <ProjectItem title="Password Manager" content={PasswordGeneratorContent}></ProjectItem>
+                        <ProjectItem title="Workload Generator" content={WorkloadGeneratorContent}></ProjectItem>
+                        <ProjectItem title="WEBSITE"></ProjectItem>
+                    </InfiniteCarousel>    
                 </div>
             </div>
         );
